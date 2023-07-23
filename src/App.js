8d39a1bad2c01094c5508822203f7a7cae3b18c4
MarcoB95-lab <sequaja.marco@gmail.com>
2023-07-24 01:55:25 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import DotGroup from "./scenes/DotGroup";
+import MySkills from "./scenes/MySkills";
+import Landing from "./scenes/Landing"
+import useMediaQuery from "./hooks/useMediaQuery";
+import LineGradient from "./components/LineGradient"
+import Navbar from "./scenes/Navbar"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  //state that determines which part of the website we on
+  const [selectedPage, setSelectedPage] = useState('home');
+  const isDesktop = useMediaQuery("(min-width: 1060px)");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true);
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (<div className="app bg-deep-blue">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+       <div className="w-5/6 mx-auto md:h-full">
+        {isDesktop && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+        <Landing setSelectedPage={setSelectedPage} />
+        </div>
+        <LineGradient />
+        <div className="w-5/6 mx-auto md:h-full">
+          <MySkills />
+        </div>
+  </div>
   );
 }
 
